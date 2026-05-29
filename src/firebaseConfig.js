e759b86d2889fdd02ad20b7596e33c22138f4933
jangheonyeong <wkdgjsdud@snu.ac.kr>
2026-05-29 19:23:10 +0900

    260529(2)
    firebaseConfig.js 추가

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB9NGReGa6CPsFXbAyv3CpyvZcsbgXbUsQ",
+  authDomain: "graduation-64244.firebaseapp.com",
+  projectId: "graduation-64244",
+  storageBucket: "graduation-64244.firebasestorage.app",
+  messagingSenderId: "783174338443",
+  appId: "1:783174338443:web:525e86f8571f5e005fd18e",
+  measurementId: "G-SEVPFH9ZET"
+};
